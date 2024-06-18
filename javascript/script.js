98175583f47bbcd856693f5ce76bd7282f12f81f
conecttheo <theodamata@gmail.com>
2024-06-18 16:56:55 -0300

    páginas de arquivo

--- javascript/script.js
+++ javascript/script.js
@@ -1,65 +1,88 @@
 let currentEditItem = null;
 
 function removerItem(event) {
-  // Prevenir o comportamento padrão do clique
-  event.preventDefault();
-  // Remover o item de lista completo (li)
-  event.target.closest('li').remove();
+    event.preventDefault();
+    event.target.closest('li').remove();
 }
 
 function addToList() {
-  // Obter os valores dos inputs
-  const nameInput = document.getElementById('nameInput').value;
-  const fileInputElement = document.getElementById('fileInput');
-  const file = fileInputElement.files[0];
+    const nameInput = document.getElementById('nameInput').value;
+    const fileInputElement = document.getElementById('fileInput');
+    const file = fileInputElement.files[0];
 
-  // Verificar se os campos não estão vazios
-  if (nameInput === '' || fileInput === '') {
-    alert('Por favor, preencha ambos os campos.');
-    return;
-  }
+    if (!nameInput || !file) {
+        alert('Por favor, preencha ambos os campos.');
+        return;
+    }
 
-  const fileURL = URL.createObjectURL(file);
+    const fileURL = URL.createObjectURL(file);
 
-  // Criar um novo item de lista (li)
-  var li = document.createElement('li');
-  li.className = 'list-group-item d-flex justify-content-between align-items-center list-group-item-action';
+    var li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center list-group-item-action';
 
-  const linkContainer = document.createElement('div');
-  linkContainer.className = 'link-container';
+    const linkContainer = document.createElement('div');
+    linkContainer.className = 'link-container';
 
-  // Criar um link e adicionar a lista
-  const link = document.createElement('a');
-  link.href = fileURL;
-  link.textContent = nameInput;
-  link.className = 'text-decoration-none';
-  link.target = '_blank';
+    const link = document.createElement('a');
+    link.href = fileURL;
+    link.textContent = nameInput;
+    link.className = 'text-decoration-none';
+    link.target = '_blank';
 
-  // Criar o botão de remoção e edição
-  const botaoEditar = document.createElement('button');
-  botaoEditar.type = 'button';
-  botaoEditar.className = 'btn btn-outline-primary ms-2';
-  botaoEditar.innerHTML = '<i class="bi bi-pencil-fill"></i>';
-  botaoEditar.addEventListener('click', () => editarItem(li, link));
+    const botaoEditar = document.createElement('button');
+    botaoEditar.type = 'button';
+    botaoEditar.className = 'btn btn-outline-primary ms-2';
+    botaoEditar.innerHTML = '<i class="bi bi-pencil-fill"></i>';
+    botaoEditar.addEventListener('click', () => editarItem(li, link));
 
-  const botaoRemover = document.createElement('button');
-  botaoRemover.type = 'button';
-  botaoRemover.className = 'btn btn-outline-danger ms-2';
-  botaoRemover.innerHTML = '<i class="bi bi-trash3-fill"></i>';
-  botaoRemover.addEventListener('click', removerItem);
+    const botaoRemover = document.createElement('button');
+    botaoRemover.type = 'button';
+    botaoRemover.className = 'btn btn-outline-danger ms-2';
+    botaoRemover.innerHTML = '<i class="bi bi-trash3-fill"></i>';
+    botaoRemover.addEventListener('click', removerItem);
 
-  linkContainer.appendChild(link);
-  li.appendChild(linkContainer);
-  li.appendChild(botaoEditar);
-  li.appendChild(botaoRemover);
+    linkContainer.appendChild(link);
+    li.appendChild(linkContainer);
+    li.appendChild(botaoEditar);
+    li.appendChild(botaoRemover);
 
-  const list = document.getElementById('list');
-  list.insertBefore(li, list.firstChild);
-  //document.getElementById('list').appendChild(li);
+    const list = document.getElementById('list');
+    list.insertBefore(li, list.firstChild);
 
-  document.getElementById('nameInput').value = '';
-  fileInputElement.value = '';
+    document.getElementById('nameInput').value = '';
+    fileInputElement.value = '';
 }
 
+function editarItem(li, link) {
+    currentEditItem = li;
 
+    document.getElementById('editNameInput').value = link.textContent;
+    document.getElementById('editFileInput').value = '';
 
+    var editModal = new bootstrap.Modal(document.getElementById('editModal'), {});
+    editModal.show();
+}
+
+document.getElementById('saveChanges').addEventListener('click', () => {
+    if (currentEditItem) {
+        const newFileInputElement = document.getElementById('editFileInput');
+        const newFile = newFileInputElement.files[0];
+        const newNameInput = document.getElementById('editNameInput').value;
+
+        if (!newNameInput) {
+            alert('Por favor, preencha o campo do nome.');
+            return;
+        }
+
+        const link = currentEditItem.querySelector('a');
+        link.textContent = newNameInput;
+
+        if (newFile) {
+            const newFileURL = URL.createObjectURL(newFile);
+            link.href = newFileURL;
+        }
+
+        var editModal = bootstrap.Modal.getInstance(document.getElementById('editModal'));
+        editModal.hide();
+    }
+});
